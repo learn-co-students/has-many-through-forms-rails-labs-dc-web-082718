f760bd7fb661b6084a66b10f8cdbd89f41470e43
github username <github email address>
2018-09-26 22:45:48 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user,
+  reject_if: Proc.new{|user_attributes| user_attributes[:username].blank?}
+
+  def username
+    self.user.username
+  end
 
 end
